57cc9f441d5d3fa4dd51b5b40fcdf95423f26464
AnjanaMenonCherubala <54480474+AnjanaMenonCherubala@users.noreply.github.com>
2020-04-13 18:49:41 -0700

    Adding online user feature

--- Homework3/index.js
+++ Homework3/index.js
@@ -1,16 +1,55 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var users = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+const getOnlineUsers = () => {
+  let clients = io.sockets.clients().connected;
+  let sockets = Object.values(clients);
+  let users = sockets.map(s => s.user);
+  return users.filter(u => u != undefined);
+};
+
 io.on('connection', function(socket){
+  const emitOnlineUsers = () => {
+      socket.broadcast.emit("users", getOnlineUsers());
+    };
+   socket.on("add_user", user => {
+       socket.emit("server_message", {
+         name: website,
+         message: `Welcome to ${website} chat !`
+       });
+
+       socket.broadcast.emit("server_message", {
+         name: website,
+         message: `${user.name} just Joined Chat`
+       });
+
+       socket.user = user;
+       emitOnlineUsers();
+     });
+
   socket.on('chat message', function(msg){
     socket.broadcast.emit('chat message', msg);
   });
   socket.broadcast.emit('hi');
+
+     socket.on("disconnect", function() {
+        const { user } = socket;
+
+        if (user) {
+          socket.broadcast.emit("server_message", {
+            name: website,
+            message: `${user.name} just left chat`
+          });
+        }
+
+        emitOnlineUsers();
+      });
 });
 
 http.listen(3000, function(){
